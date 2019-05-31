e5d00e08257b61182233d97438ae6b3ee78f040b
Victor Manuel Sanchez Sanchez <vicool275@gmail.com>
2019-05-31 02:57:13 -0500

    Version Final v1

--- models/videogame.js
+++ models/videogame.js
@@ -4,8 +4,14 @@ var Schema = mongoose.Schema;
 var VideogameSchema =  Schema({
   identifier:Number,
   title:String,
-  plataform:String,
-  since:String
+  plataform:{
+    Nintendo:String,
+    PC:String,
+    Sony:String,
+    Microsoft:String
+  },
+  since:String,
+  image:String
 });
 
 module.exports = mongoose.model('videojuegos', VideogameSchema);
--- routes/users.js
+++ routes/users.js
@@ -28,8 +28,14 @@ router.post('/', function(req, res, next) {
   var videojuego = VideoGame({
     identifier: req.body.identifier,
     title: req.body.title,
-    plataform: req.body.plataform,
-    since: req.body.since
+    plataform: {
+      Nintendo: req.body.nintendo,
+      PC: req.body.pc,
+      Sony: req.body.sony,
+      Microsoft: req.body.microsoft
+    },
+    since: req.body.since,
+    image: req.body.image
   });
 
   videojuego.save(function(err, data) {
@@ -60,7 +66,11 @@ router.delete('/',function(req,res,next){
 });
 
 router.patch('/:userId', function(req, res, next) {
-  VideoGame.findOneAndUpdate({identifier: req.params.userId},{$set:{identifier:50}},
+  VideoGame.findOneAndUpdate({
+    identifier: req.params.userId
+  },{
+    $set:{identifier: 50 + req.params.userId}
+  },
   function(err, datos) {
     if (datos == null) {
       res.status(404).json({
@@ -68,15 +78,44 @@ router.patch('/:userId', function(req, res, next) {
       });
     } else {
       res.status(200).json(datos);
-      {mensaje: "se ha cambiado exitosamente el ID"}
     }
-
   });
-
 });
+
 router.patch('/',function(req,res,next){
   res.status(405).json({mensaje:'Accion no permitida'});
 });
 
+router.put('/:userId', function(req, res, next) {
+  VideoGame.replaceOne({
+    identifier: req.params.userId
+  },{
+    identifier: req.body.identifier,
+    title: req.body.title,
+    plataform: {
+      Nintendo: req.body.nintendo,
+      PC: req.body.pc,
+      Sony: req.body.sony,
+      Microsoft: req.body.microsoft
+    },
+    since: req.body.since,
+    image: req.body.image
+  },
+  function(err, datos) {
+    if (datos == null) {
+      res.status(404).json({
+        mensaje: "No existe"
+      });
+    } else {
+      res.status(200).json({
+        mensaje: "se ha cambiado exitosamente el VideoJuego"
+      });
+    }
+  });
+});
+
+router.put('/',function(req,res,next){
+  res.status(405).json({mensaje:'Accion no permitida'});
+});
 
 module.exports = router;
